38bc0be62192ad36990f29f8a6250368b7f06a8d
Alex Barkell <albarkel@umich.edu>
2022-02-11 17:49:15 -0500

    WIP - add more layout and styles

--- src/components/SpatialViewer/ImageDatasetList.js
+++ src/components/SpatialViewer/ImageDatasetList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Col, Container, Row } from "reactstrap";
 import { getSpatialDataAsJSON } from "../../helpers/dataHelper";
-import { getDerivedImageName, getImageTypeTooltipCopy } from "./viewConfigHelper";
+import { getImageTypeTooltipCopy } from "./viewConfigHelper";
 import {
     SortingState,
     IntegratedSorting,
@@ -42,29 +42,20 @@ class ImageDatasetList extends Component {
                 getCellValue: row => <button onClick={() => this.props.setSelectedImageDataset(row)} type='button' className='table-column btn btn-link text-left p-0'>{row["Participant ID"]}</button>
             },
             { name: 'Data Type', title: 'DATA TYPE' },
-            { name: 'Tissue Type', title: 'TISSUE TYPE' },
             {
                 name: 'Image Type',
                 title: 'IMAGE TYPE',
                 getCellValue: this.getImageTypeCell
             },
             {
-                name: 'Level',
-                title:
-                    <span>LEVEL&nbsp;
-                        <span className='tooltip-icon'>
-                        <span className="icon-info"><i className="fas fa-info-circle"></i></span>
-                        <div className='tooltip-parent rounded border shadow-sm mt-1 p-2'>
-                            <span className='tooltip-child'>Identifier of the section of the FFPE tissue block used in light microscopy.</span>
-                        </div>
-                        </span>
+                name: 'Info',
+                title: '',
+                getCellValue: row => { 
+                    return <span className="icon-info">
+                     <i className="fas fa-info-circle"></i>
                     </span>
+                }
             },
-            {
-                name: 'Source File',
-                title: 'FILE NAME',
-                getCellValue: row => getDerivedImageName(row["Source File"])
-            }
         ];
     };
 
@@ -72,9 +63,6 @@ class ImageDatasetList extends Component {
         return getImageTypeTooltipCopy(row["Image Type"]) !== "" &&
                 <div>
                         <span className='mr-1'>{row["Image Type"]}</span>
-                    <span className="icon-info">
-                        <i className="fas fa-info-circle"></i>
-                    </span>
                         <div className='tooltip-parent rounded border shadow-sm mt-1 p-2'>
                             <span className='tooltip-child'>{getImageTypeTooltipCopy(row["Image Type"])}</span>
                         </div>
@@ -84,28 +72,96 @@ class ImageDatasetList extends Component {
     getDefaultColumnWidths = () => {
         return [
             { columnName: 'Participant ID', width: 120 },
-            { columnName: 'Data Type', width: 155 },
-            { columnName: 'Tissue Type', width: 100 },
-            { columnName: 'Image Type', width: 270 },
-            { columnName: 'Level', width: 100 },
-            { columnName: 'Source File', width: 250 }
+            { columnName: 'Data Type', width: 250 },
+            { columnName: 'Image Type', width: 650 },
+            { columnName: 'Info', width: 25 },
         ]
     };
 
     render() {
         return (
-            <Container id='outer-wrapper' className="multi-container-container">
+            <Container id='outer-wrapper' className="multi-container-container container-xxl">
                 <Row>
                     <Col md={12}>
-                        <Container className="mt-3 rounded border p-3 shadow-sm">
-                            <Row><Col><h5>Welcome to the Kidney Tissue Atlas Spatial Viewer (beta)</h5></Col></Row>
+                        <Container className="mt-2 rounded border p-3 shadow-sm container-max">
+                            <Row><Col><h5>Welcome to the Kidney Tissue Atlas Spatial Viewer</h5></Col></Row>
                             <Row><Col><p>Select a spatial dataset from the list below to visualize it in the <a target="_blank" rel="noreferrer" href="http://vitessce.io/">Vitessce</a> visual integration tool.</p></Col></Row>
                         </Container>
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={12}>
-                        <Container className='rounded border shadow-sm my-3 p-3 overflow-auto'>
+                    <Col md={3}>
+                        <Container className="mt-3 rounded border p-3 shadow-sm spatial-data-table container-max">
+                            <Row><Col><h5>Data Type</h5></Col></Row>
+                            <Row><Col><h5>Image Type</h5></Col></Row>
+                            <Row><Col><h5>Sample Type</h5></Col></Row>
+                        </Container>
+                    </Col>
+                    <Col md={9}>
+                        <Row>
+                            <Col className='my-3 p-3'>
+                                <div className="border rounded activeFilter">
+                                    <span>
+                                        Active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+
+                                <div className="border rounded activeFilter ">
+                                    <span>
+                                        Active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+                                
+                                <div className="border rounded activeFilter ">
+                                    <span>
+                                        Active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+                                
+                                <div className="border rounded activeFilter ">
+                                    <span>
+                                        Really long active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+                                <div className="border rounded activeFilter ">                                    
+                                    <span>
+                                        Really long active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+                                <div className="border rounded activeFilter ">                                    
+                                    <span>
+                                        Really long active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+                                <div className="border rounded activeFilter ">                                    
+                                    <span>
+                                        Really long active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+                                <div className="border rounded activeFilter ">                                    
+                                    <span>
+                                        Really long active filter appears here
+                                        &nbsp; &nbsp; &nbsp;
+                                        <i className="close-button fas fa-xmark"></i>
+                                    </span>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Container className='rounded border shadow-sm my-3 p-3 overflow-auto container-max'>
                             <div className="spatial-data-table">
                                 <Grid
                                     rows={this.state.tableData}
