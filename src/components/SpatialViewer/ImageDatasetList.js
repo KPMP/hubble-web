352a83827a9ef3b6b9cd6a14fb2e4d691dc7e6ca
Alex Barkell <albarkel@umich.edu>
2022-02-14 14:17:00 -0500

    add layout styles with simple toggles

--- src/components/SpatialViewer/ImageDatasetList.js
+++ src/components/SpatialViewer/ImageDatasetList.js
@@ -24,6 +24,8 @@ class ImageDatasetList extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            filterTabActive: true,
+            activeFilterTab: 'DATASET',
             tableData: []
         }
     }
@@ -78,11 +80,27 @@ class ImageDatasetList extends Component {
         ]
     };
 
+    toggleFilterTab = () => {
+        if(this.state.filterTabActive) {
+            this.setState({filterTabActive: false});
+        } else {
+            this.setState({filterTabActive: true});
+        }
+    }
+    setActiveFilterTab = (tabName) => {
+        console.log('foo',tabName)
+        this.setState({activeFilterTab: tabName});
+    }
+    
     render() {
+        const tabEnum = {
+            DATASET: 'DATASET',
+            PARTICIPANT: 'PARTICIPANT',
+        }
         return (
             <Container id='outer-wrapper' className="multi-container-container container-xxl">
                 <Row>
-                    <Col md={12}>
+                    <Col xl={12}>
                         <Container className="mt-2 rounded border p-3 shadow-sm container-max">
                             <Row><Col><h5>Welcome to the Kidney Tissue Atlas Spatial Viewer</h5></Col></Row>
                             <Row><Col><p>Select a spatial dataset from the list below to visualize it in the <a target="_blank" rel="noreferrer" href="http://vitessce.io/">Vitessce</a> visual integration tool.</p></Col></Row>
@@ -90,14 +108,29 @@ class ImageDatasetList extends Component {
                     </Col>
                 </Row>
                 <Row>
-                    <Col md={3}>
-                        <Container className="mt-3 rounded border p-3 shadow-sm spatial-data-table container-max">
+                    <Col xl={3}>
+                        <div className="filter-panel-wrapper">
+                        <div className="filter-panel-tab-wrapper">
+                            <div onClick={() => {this.setActiveFilterTab(tabEnum.DATASET)}}
+                                className={`filter-tab ${this.state.activeFilterTab === tabEnum.DATASET ? 'active' : ''} rounded border`}>DATASET</div>
+                            <div onClick={() => {this.setActiveFilterTab(tabEnum.PARTICIPANT)}}
+                                className={`filter-tab ${this.state.activeFilterTab === tabEnum.PARTICIPANT ? 'active' : ''} rounded border`}>PARTICIPANT</div>
+                            
+                            <div className="filter-tab filter-tab-control-icon">
+                                {this.state.filterTabActive
+                                    ?<i onClick={() => {this.toggleFilterTab()}} className="fas fa-angles-left"></i>
+                                    :<i onClick={() => {this.toggleFilterTab()}} className="fas fa-angles-right"></i>
+                                }
+                            </div>
+                        </div>
+                        <Container className="mt-3 rounded border p-3 shadow-sm spatial-filter-panel container-max">
                             <Row><Col><h5>Data Type</h5></Col></Row>
                             <Row><Col><h5>Image Type</h5></Col></Row>
                             <Row><Col><h5>Sample Type</h5></Col></Row>
                         </Container>
+                        </div>
                     </Col>
-                    <Col md={9}>
+                    <Col xl={9}>
                         <Row>
                             <Col className='my-3 p-3'>
                                 <div className="border rounded activeFilter">
@@ -161,7 +194,7 @@ class ImageDatasetList extends Component {
                                 </div>
                             </Col>
                         </Row>
-                        <Container className='rounded border shadow-sm my-3 p-3 overflow-auto container-max'>
+                        <Container className='rounded border shadow-sm p-3 overflow-auto container-max'>
                             <div className="spatial-data-table">
                                 <Grid
                                     rows={this.state.tableData}
