7a827a05534a37d0718e5b08537e0110c0790459
Alex Barkell <albarkel@umich.edu>
2022-02-22 15:56:59 -0500

    add filter

--- src/components/SpatialViewer/Plugins/ColumnArrangementDialog.js
+++ src/components/SpatialViewer/Plugins/ColumnArrangementDialog.js
@@ -4,8 +4,15 @@ function useForceUpdate(){
   return () => setValue(value => value + 1);
 }
 
+
 function ColumnArrangementDialog(props) {
   let forceUpdate = useForceUpdate();
+  const [filterValue, setFilterValue] = useState('');
+
+  const onChangeHandler = event => {
+    setFilterValue(event.target.value);
+  };
+  
   return(
     <div className='column-arrage-dialog'>
     {(props.arrangeColumnsDialogOpen) &&
@@ -16,16 +23,21 @@ function ColumnArrangementDialog(props) {
                 <i alt="Arrange Columns" className="fas fa-magnifying-glass"></i>
                 <input
                 type="text"
+                value={filterValue}
+                onChange={onChangeHandler}
                 placeholder="Filter Columns"
                 />
               </div>
               <div className="sort-dialog-options fake-link">
-                <span>Restore Defaults</span>
+                <span onClick={()=>{setFilterValue('')}}>Restore Defaults</span>
               </div>
+       
               {props.toolbarColumns && props.toolbarColumns.length > 0 ?
                 <div className="dialog-content" onClick={e => {e.stopPropagation();}}>
                   {props.toolbarColumns.map((item, index) => { 
+
                     return (
+                      <div>{ ((item.name).toLowerCase()).indexOf(filterValue.toLowerCase()) == -1 ? <div></div> :
                       <div className="sort-dialog-option-wrapper">
                         <div className="sort-dialog-options">
                           <input onClick={(e)=> {
@@ -48,6 +60,8 @@ function ColumnArrangementDialog(props) {
                         <div className="sort-dialog-radio-wrapper">
                           <i alt="Arrange Columns" className="fas fa-bars"></i>
                         </div>
+                      </div>
+                        }
                       </div>
                     )
                   })}
