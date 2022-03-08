fd7ada523a0e4954441c49cc5275956d943ef168
Alex Barkell <albarkel@umich.edu>
2022-03-08 16:14:21 -0500

    remove unused function

--- src/helpers/dataHelper.js
+++ src/helpers/dataHelper.js
@@ -1,21 +1,6 @@
-import { readRemoteFile } from 'react-papaparse';
-
-export const getSpatialDataAsJSON = async () => {
-    return new Promise(resolve => {
-        readRemoteFile(process.env.REACT_APP_SPATIAL_DATA_PATH, {
-            download: true,
-            header: true,
-            complete: (results) => {
-                resolve(results.data);
-            }
-        });
-    });
-};
-
 export const resultConverter = (results) => {
     return results.map(row => {
         return Object.keys(row).reduce((attrs, key)=> ({...attrs, [key]: row[key].raw}), {});
     })
 };
-
 
