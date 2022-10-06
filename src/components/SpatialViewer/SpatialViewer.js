e7334c3b3b60ea46557a69f3c80c497da2e557cc
Alex Barkell <albarkel@umich.edu>
2022-10-06 16:58:09 -0400

    Add base fly-out

--- src/components/SpatialViewer/SpatialViewer.js
+++ src/components/SpatialViewer/SpatialViewer.js
@@ -6,6 +6,8 @@ import { getViewConfig, populateViewConfig } from './viewConfigHelper';
 import { createHeaderString } from './spatialHelper';
 import { Redirect } from 'react-router-dom';
 import { handleGoogleAnalyticsEvent } from "../../helpers/googleAnalyticsHelper";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
 
 
 class SpatialViewer extends Component {
@@ -16,6 +18,7 @@ class SpatialViewer extends Component {
             viewConfig: '',
             noData: true,
             headerString: '',
+            reportCardOpen: false
         }
     }
 
@@ -32,6 +35,10 @@ class SpatialViewer extends Component {
         }
     }
 
+    toggleReportCard = () => {
+        this.setState({reportCardOpen: !this.state.reportCardOpen})
+    }
+
     render() {
         if (!this.props.selectedImageDataset || (this.props.selectedImageDataset && Object.keys(this.props.selectedImageDataset).length === 0)) {
             return <Redirect to='/' />
@@ -40,16 +47,35 @@ class SpatialViewer extends Component {
         return (
             <div className="container-fluid">
                 <div id="vitessce-container" className="rounded border shadow-sm mt-2 mx-3 p-3">
-                {!this.state.noData &&
-                    <div>
-                <Row xs='12'>
-                    <Col xs='8'><h5>
-                        {this.state.headerString}
-                    </h5></Col>
-                    <Col xs='4' className="text-right text-primary ">
-                        <button onClick={() => {this.props.history.goBack()}} type='button' className='btn btn-link'>
-                            <h5><span style={{"font-size":"26px"}}>&larr;</span> Close viewer</h5></button></Col>
-                </Row>
+                    
+                    <div id="side-menu" className={`shadow-sm border ${this.state.reportCardOpen ? "menu-open" : "menu-closed"}`}>
+                        <div className="menu-slide-list-header">
+                            <Row>
+                                <Col className="menu-title">
+                                    <div className="menu-header-close-button clickable" onClick={()=>{this.toggleReportCard()}}>
+                                        <FontAwesomeIcon className={"fa fa-angle-down"} icon={faChevronRight} />
+                                        <FontAwesomeIcon className={"fa fa-angle-down"} icon={faChevronRight} />
+                                    </div>
+                                    <span>Participant Information</span>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+
+
+                    {!this.state.noData &&
+                        <div>
+                    <Row xs='12'>                        
+                        <Col xs='8'>
+                            <h5 onClick={()=>{this.toggleReportCard()}}>
+                                {this.state.headerString}
+                            </h5>
+                        </Col>
+                        <Col xs='4' className="text-right text-primary ">
+                            <button onClick={() => {this.props.history.goBack()}} type='button' className='btn btn-link'>
+                                <h5><span style={{"font-size":"26px"}}>&larr;</span> Close viewer</h5></button></Col>
+                    </Row>
+                    
                     <Vitessce
                     config={this.state.viewConfig}
                     height={window.innerHeight - 200}
