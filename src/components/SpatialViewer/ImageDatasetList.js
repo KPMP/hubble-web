666cffffe9d78cf7a569177d2daaa11e106b2041
Dert1129 <nathancreger1111@gmail.com>
2023-04-21 11:06:56 -0400

    wrap in div

--- src/components/SpatialViewer/ImageDatasetList.js
+++ src/components/SpatialViewer/ImageDatasetList.js
@@ -259,8 +259,11 @@ class ImageDatasetList extends Component {
                             <Container className="mt-3 rounded border p-3 shadow-sm spatial-filter-panel container-max">
                                 <Row className="mb-2"><Col><Facet field="datatype" label="Experimental Strategy" filterType="any" show="10"
                                                                   view={MultiCheckboxFacet}/></Col></Row>
-                                <span height="100vh"><Row className="mb-2"><Col><Facet field="imagetype" label="Image Type" filterType="any" show="10"
-                                                                  view={MultiCheckboxFacet}/></Col></Row></span>
+                                <div id="image_type">
+                                    <Row className="mb-2"><Col><Facet field="imagetype" label="Image Type" filterType="any" show="10"
+                                                                    view={MultiCheckboxFacet}/></Col></Row>
+                                </div>
+                                
                             </Container>
                             }{this.props.activeFilterTab === tabEnum.PARTICIPANT &&
                         <Container className="mt-3 rounded border p-3 shadow-sm spatial-filter-panel container-max">
