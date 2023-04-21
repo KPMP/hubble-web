ba5f7594e5187cb219e413dd9393545b733504df
Dert1129 <nathancreger1111@gmail.com>
2023-04-21 10:35:22 -0400

    hardcode height

--- src/components/SpatialViewer/ImageDatasetList.js
+++ src/components/SpatialViewer/ImageDatasetList.js
@@ -259,8 +259,8 @@ class ImageDatasetList extends Component {
                             <Container className="mt-3 rounded border p-3 shadow-sm spatial-filter-panel container-max">
                                 <Row className="mb-2"><Col><Facet field="datatype" label="Experimental Strategy" filterType="any" show="10"
                                                                   view={MultiCheckboxFacet}/></Col></Row>
-                                <Row className="mb-2"><Col><Facet field="imagetype" label="Image Type" filterType="any" show="10"
-                                                                  view={MultiCheckboxFacet}/></Col></Row>
+                                <span height="100vh"><Row className="mb-2"><Col><Facet field="imagetype" label="Image Type" filterType="any" show="10"
+                                                                  view={MultiCheckboxFacet}/></Col></Row></span>
                             </Container>
                             }{this.props.activeFilterTab === tabEnum.PARTICIPANT &&
                         <Container className="mt-3 rounded border p-3 shadow-sm spatial-filter-panel container-max">
