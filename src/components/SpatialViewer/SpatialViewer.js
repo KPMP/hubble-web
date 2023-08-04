827e3189a4ce8970c851b8fa2218b23d28af746b
dert1129 <nathancreger1111@gmail.com>
2023-08-04 14:19:21 -0400

    improve logging

--- src/components/SpatialViewer/SpatialViewer.js
+++ src/components/SpatialViewer/SpatialViewer.js
@@ -29,9 +29,10 @@ class SpatialViewer extends Component {
                 this.props.selectedImageDataset["spectrackSampleId"] 
                 + this.props.selectedImageDataset['imagetype']);
             let viewConfig = getViewConfig(this.props.selectedImageDataset["configtype"]);
+            console.log("View config beofre population " + viewConfig);
             viewConfig = await populateViewConfig(viewConfig, this.props.selectedImageDataset);
-            console.log("View config in Sv: "+ viewConfig);
             this.setState({viewConfig: viewConfig, noData: false});
+            console.log("View config after population"+ viewConfig);
         }
     }
 
