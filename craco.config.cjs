6f2d1228ee598c3bb55bbb7f3fd8630368198d05
dert1129 <nathancreger1111@gmail.com>
2023-09-06 14:02:37 -0400

    add config

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,5 +1,5 @@
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
- 
+const CracoAlias = require("craco-alias");
 const isProductionBuild = process.env.NODE_ENV === "production"
 const shouldAnalyze = process.env.REACT_APP_RUN_ANALYZER === "true"
 const plugins = []
@@ -7,6 +7,18 @@ if (isProductionBuild && shouldAnalyze ) {
   plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server' }))
 }
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "options",
+        baseUrl: "./",
+        aliases: {
+          "txml/txml": "./node_modules/txml/dist/txml",
+        }
+      }
+    }
+  ],
   webpack: {
     plugins,
     configure: {
