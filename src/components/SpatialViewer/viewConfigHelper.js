60fafbee193f08881d4c82a179dbf0d33da37d0b
HaneenT <haneen@umich.edu>
2024-08-27 11:54:55 -0400

    fix test script

--- src/components/SpatialViewer/viewConfigHelper.js
+++ src/components/SpatialViewer/viewConfigHelper.js
@@ -50,14 +50,16 @@ export const populateViewConfig = async (viewConfig, selectedDataset) => {
         });
         let dataUrl = getPublicFileLink(selectedDataset["packageid"], relatedFiles[0]['filename']);
         stringifiedConfig = stringifiedConfig.replace(/<DATA_FILE_URL>/gi, dataUrl);
-        stringifiedConfig = stringifiedConfig.replace('<SEGMENTATION_MASK_NAME>', relatedFiles[0]['filename']);
-        stringifiedConfig = stringifiedConfig.replace('<SEGMENTATION_MASK_URL>', dataUrl);
 
-        const loaders = await loadOmeTiff(dataUrl);
-        const physicalSizeX = unit(loaders.metadata.Pixels.PhysicalSizeX, (loaders.metadata.Pixels.PhysicalSizeXUnit.replace('µ', 'u'))).to("um").toNumber();
-        const physicalSizeY = unit(loaders.metadata.Pixels.PhysicalSizeY, (loaders.metadata.Pixels.PhysicalSizeYUnit.replace('µ', 'u'))).to("um").toNumber();
-        stringifiedConfig = stringifiedConfig.replace('"<PHYSICAL_SIZE_X>"', physicalSizeX);
-        stringifiedConfig = stringifiedConfig.replace('"<PHYSICAL_SIZE_Y>"', physicalSizeY);
+        if (selectedDataset["configtype"] === "Segmentation") {
+            stringifiedConfig = stringifiedConfig.replace('<SEGMENTATION_MASK_NAME>', relatedFiles[0]['filename']);
+            stringifiedConfig = stringifiedConfig.replace('<SEGMENTATION_MASK_URL>', dataUrl);
+            const loaders = await loadOmeTiff(dataUrl);
+            const physicalSizeX = unit(loaders.metadata.Pixels.PhysicalSizeX, (loaders.metadata.Pixels.PhysicalSizeXUnit.replace('µ', 'u'))).to("um").toNumber();
+            const physicalSizeY = unit(loaders.metadata.Pixels.PhysicalSizeY, (loaders.metadata.Pixels.PhysicalSizeYUnit.replace('µ', 'u'))).to("um").toNumber();
+            stringifiedConfig = stringifiedConfig.replace('"<PHYSICAL_SIZE_X>"', physicalSizeX);
+            stringifiedConfig = stringifiedConfig.replace('"<PHYSICAL_SIZE_Y>"', physicalSizeY);
+        }
     }
     stringifiedConfig = stringifiedConfig.replace('<IMAGE_NAME>', selectedDataset["filename"]);
     stringifiedConfig = stringifiedConfig.replace('<IMAGE_URL>', imageUrlResponse.data);
