77674e1b29c4a7c0b71767799fa0cb4f5244e60f
dert1129 <nathancreger1111@gmail.com>
2025-07-18 09:47:57 -0400

    edit craco config

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,11 +1,15 @@
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const CracoAlias = require("craco-alias");
-const isProductionBuild = process.env.NODE_ENV === "production"
-const shouldAnalyze = process.env.REACT_APP_RUN_ANALYZER === "true"
-const plugins = []
-if (isProductionBuild && shouldAnalyze ) {
-  plugins.push(new BundleAnalyzerPlugin({ analyzerMode: 'server' }))
+
+const isProductionBuild = process.env.NODE_ENV === "production";
+const shouldAnalyze = process.env.REACT_APP_RUN_ANALYZER === "true";
+
+const plugins = [];
+
+if (isProductionBuild && shouldAnalyze) {
+  plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "server" }));
 }
+
 module.exports = {
   plugins: [
     {
@@ -15,28 +19,58 @@ module.exports = {
         baseUrl: "./",
         aliases: {
           "txml/txml": "./node_modules/txml/dist/txml",
-        }
-      }
-    }
-  ],
-  webpack: {
-    plugins,
-    configure: {
-      module: {
-        rules: [
-          {
-            test: /\.m?js/,
-            resolve: {
-              fullySpecified: false
-            }
-          },
-        ],
-        parser: {
-          javascript: {
-            exportsPresence: false,
-          },
         },
       },
     },
+  ],
+  webpack: {
+    plugins,
+    configure: (webpackConfig) => {
+      if (isProductionBuild) {
+        webpackConfig.devtool = false;
+      }
+
+      webpackConfig.module.rules.forEach((rule) => {
+        if (rule.oneOf) {
+          rule.oneOf.forEach((subRule) => {
+            if (subRule.use && Array.isArray(subRule.use)) {
+              subRule.use = subRule.use.filter(
+                (u) =>
+                  !(
+                    typeof u === "object" &&
+                    u.loader &&
+                    u.loader.includes("thread-loader")
+                  )
+              );
+            }
+          });
+        }
+      });
+
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        (plugin) => plugin.constructor.name !== "ForkTsCheckerWebpackPlugin"
+      );
+
+      webpackConfig.optimization.splitChunks = {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 20000,
+      };
+
+      webpackConfig.module.rules.push({
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
+      });
+
+      webpackConfig.module.parser = {
+        javascript: {
+          exportsPresence: false,
+        },
+      };
+
+      return webpackConfig;
+    },
   },
-}
+};
